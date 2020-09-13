a4c031ad17cc7be0940f94b74c1d71a4578f86d5
335015939 <yuming.zhou@g.feitianacademy.org>
2020-09-13 14:52:58 +0000

    added some structs

--- structs.h
+++ structs.h
@@ -5,4 +5,28 @@ typedef struct {
     int f;
     int b;
 }COLOR_PAIR_DEF;
+typedef struct {
+    int id;
+    int num;
+} ITEM;
+typedef struct {
+    int id[5];
+    int low[5];
+    int max[5];
+    int chance[5];
+}DROPITEMDATA;
+typedef struct {
+    int c;
+    int a;
+    int b;
+    int cp;
+} TILE;
+typedef struct {
+    int id;
+    char passable;
+    char minable;
+    char droppable;
+    TILE t;
+    DROPITEMDATA drops;
+} TILEDATA;
 #endif
