210c69fca6cabd2f9cd7eb30c92d322d093002ea
335015939 <yuming.zhou@g.feitianacademy.org>
2020-09-15 13:47:28 +0000

    ???

--- mapgen.c
+++ mapgen.c
@@ -1,12 +1,20 @@
+#include "funcs.h"
 #include "header.h"
 
 void mktree(){
 
 };
 
+COORDS genplains(COORDS c,int l){
+    int x=c.x;
+    for(;c.x<(x+l);c.x++){
+        putmapid(c.x, c.x, TILE_DIRT);
+    };
+};
+
 void mapgen(){
     COORDS c;
-    COORDS (*genfuncs[1])(COORDS c)={};
+    COORDS (*genfuncs[1])(COORDS c)=,int l{genplains};
     for(c.x=0;c.x<MAP_W;c.x++){
         c=(genfuncs[1])(c);
     }
