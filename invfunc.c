6d1aeb709cf9a4464840117b7507512739f9608c
335015939 <yuming.zhou@g.feitianacademy.org>
2020-09-18 18:19:02 +0000

    mostly finished inventory management

--- invfunc.c
+++ invfunc.c
@@ -3,7 +3,7 @@
 void drawitem(ITEMDATA i){
     dtile(i.t);
 };
-void drawinv(COORDS c){
+void drawinv(COORDS c,COORDS s,char f){
     int i,j;
     clear();
     attr_set(A_BOLD,0,NULL);
@@ -23,16 +23,24 @@ void drawinv(COORDS c){
     for(i=0;i<12;i++){
         addch('-');
     };
-    printw("\n%s",ITEMS[player.i[c.x][c.y].id].name);
-    attr_set(A_REVERSE,ITEMS[player.i[i][j].id].t.cp,NULL);
+    printw("\n%s(%d)",ITEMS[player.i[c.x][c.y].id].name,player.i[c.x][c.y].num);
+    attr_set(A_REVERSE,ITEMS[player.i[c.x][c.y].id].t.cp,NULL);
     mvaddch(c.y+2,c.x+1,ITEMS[player.i[c.x][c.y].id].t.c);
+    
+    if(f){
+        attr_set(A_BLINK | A_REVERSE,ITEMS[player.i[s.x][s.y].id].t.cp,NULL);
+        mvaddch(s.y+2,s.x+1,ITEMS[player.i[s.x][s.y].id].t.c);
+    };
 
 };
 void inventory(){
     int k,i,j;
     static COORDS c={};
+    COORDS s={};
+    ITEM item;
+    char f=0;
     do{
-        drawinv(c);
+        drawinv(c,s,f);
         k=getch();
         switch(k){
             case 'a':
@@ -55,6 +63,18 @@ void inventory(){
             case KEY_DOWN:
                 c.y++;
                 break;
+            case 'j':
+            case 'J':
+            case '\n':
+                if (!f){
+                    f=1;
+                    s=c;
+                }else{
+                    item=player.i[c.x][c.y];
+                    player.i[c.x][c.y]=player.i[s.x][s.y];
+                    player.i[s.x][s.y]=item;
+                    f=0;
+                };
         };
         c.x+=(c.x<0)-(c.x>9);
         c.y+=(c.y<0)-(c.y>9);
