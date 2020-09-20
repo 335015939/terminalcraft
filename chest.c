8d587fc6ebdd614405fbfb0cde059baba868241e
335015939 <yuming.zhou@g.feitianacademy.org>
2020-09-20 19:59:05 +0000

    more chest things

--- chest.c
+++ chest.c
@@ -11,7 +11,7 @@ void drawchestitem(COORDS c,int attr){
     attr_set(attr | ITEMS[(item).id].t.a,ITEMS[item.id].t.cp,NULL);
     addch(ITEMS[(item).id].t.c);
 };
-void drawchest(COORDS s,COORDS c){
+void drawchest(COORDS s,COORDS c,char f){
     int i,j;
     clear();
     addstr("Inventory   Chest\n");
@@ -33,12 +33,39 @@ void drawchest(COORDS s,COORDS c){
     for(i=0;i<23;i++){
         addch('=');
     };
+    move(c.y+2,c.x+(c.x>=10)+1);
+    drawchestitem(c,A_REVERSE);
+    if(f){
+        move(s.y+2,s.x+(s.x>=10)+1);
+        drawchestitem(s,A_REVERSE);
+    }
 };
 void chestitemswap(COORDS a,COORDS b){
-
+    ITEM (*item1);
+    ITEM (*item2);
+    ITEM (*item)=malloc_throw(sizeof(ITEM));
+    if(a.x>10){
+        item1=cheststorage[a.x-10][a.y];
+    }else{
+        item1=&(player.i[a.x][a.y]);
+    };
+    if(b.x>=10){
+        item2=cheststorage[b.x-10][b.y];
+    }else{
+        item2=&(player.i[b.x][b.y]);
+    };
+    *item=*item2;
+    *item2=*item1;
+    *item1=*item;
+    free(item);
+    clear();
+    printw("%s %s",ITEMS[(*item1).id].name,ITEMS[(*item2).id].name);
+    getch();
 };
 void chest(int x,int y){
+    int selected=1;
     cheststorage=&getmaptile(x,y).storage;
-    drawchest((COORDS){0,0},(COORDS){0,0});
+    chestitemswap((COORDS){0,0},(COORDS){10,0});
+    drawchest((COORDS){0,0},(COORDS){0,0},selected);
     getch();
 };
